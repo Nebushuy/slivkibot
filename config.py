d9c05ab358f7638d9dc64b5070e192d100dba5ff
Nebushuy <94021658+Nebushuy@users.noreply.github.com>
2024-01-30 15:19:18 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Токен бота
-TOKEN = '6933425414:AAEsJVwHhOCzfW3dhzI0swz7sI4yAmojfIg'
-
-# Идентификатор чата, в который будем отправлять сообщения
-CHAT_ID = ('-1002138041570')
-#CHAT_ID = None
-#Slivki ID -1002138041570
-#My ID 196032615
