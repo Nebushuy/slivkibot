fdf470d06d8c4abfeac382e39e6aa16619193fb0
neb <kirill.green336@gmail.com>
2024-01-30 15:11:50 +0300

    del token

---
+++ config.py
@@ -0,0 +1,8 @@
+# Токен бота
+TOKEN = '6933425414:AAEsJVwHhOCzfW3dhzI0swz7sI4yAmojfIg'
+
+# Идентификатор чата, в который будем отправлять сообщения
+CHAT_ID = ('-1002138041570')
+#CHAT_ID = None
+#Slivki ID -1002138041570
+#My ID 196032615
